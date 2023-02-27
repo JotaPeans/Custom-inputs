c524007c5754d2cc0acb63fd5e2f68a0c4365634
JotaPeans <jpfontesferreira1@gmail.com>
2023-02-27 12:02:05 -0300

    update

--- src/Input.tsx
+++ src/Input.tsx
@@ -1,16 +1,16 @@
 import React, { useState, useRef } from "react";
 
 interface IInput {
-    label: string,
+    inputType: "email" | "password" | "cpf" | "text",
     refInput: React.RefObject<HTMLInputElement>,
 }
 
-const Input = ({ label, refInput }: IInput) => {
+const Input = ({inputType = "text", refInput }: IInput) => {
     const ref = useRef<HTMLInputElement>(null);
-    const [labelState, setlabelState] = useState(false)
+    const [labelState, setlabelState] = useState(false);
 
     function handleFocus() {
-        ref.current?.value 
+        ref.current?.value;
         setlabelState(true);
     }
 
@@ -18,17 +18,37 @@ const Input = ({ label, refInput }: IInput) => {
         !ref.current?.value ? setlabelState(false) : setlabelState(true);
     }
 
+    function maskCpf() {
+        const input = ref.current;
+
+        if(input?.value.length === 3 || input?.value.length === 7) {
+            input.value = input.value + ".";
+        }
+        else if(input?.value.length === 11) {
+            input.value = input.value + "-";
+        }  
+    }
+
     function handleChange() {
+        if(inputType === "cpf") {
+            maskCpf();
+
+            if(refInput.current && ref.current) {
+                refInput.current.value = ref.current.value.replace(/\D/g, "");
+            }
+            return
+        }
+
         if(refInput.current && ref.current) {
-            refInput.current.value = ref.current.value
+            refInput.current.value = ref.current.value;
         }
     }
 
     return (
         <div className="flex flex-col justify-center relative">
-            <label className={`absolute transition-all ${labelState ? " -translate-y-11" : null} left-4 text-xl`} htmlFor={label}>{label}</label>
+            <label className={`absolute transition-all ${labelState ? " -translate-y-11" : null} left-4 text-xl ${inputType === "cpf" ? "uppercase" : "capitalize"}`} htmlFor={inputType}>{inputType}</label>
             <input className="hidden" ref={refInput} type="text" />
-            <input onChange={handleChange} onBlur={handleBlur} onFocus={handleFocus} ref={ref} className=" h-12 bg-slate-900/40 rounded-xl px-4 text-xl" id={label} type="text" />
+            <input maxLength={inputType === "cpf" ? 14 : undefined} type={inputType} onChange={handleChange} onBlur={handleBlur} onFocus={handleFocus} ref={ref} className=" h-12 bg-slate-900/40 rounded-xl px-4 text-xl" id={inputType} />
         </div>
     );
 }
