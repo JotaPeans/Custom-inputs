56bd5ad30cb7f66464ad4dab2f12974f20bea9a4
JotaPeans <jpfontesferreira1@gmail.com>
2023-03-05 23:52:33 -0300

    update input.tsx

--- src/Input.tsx
+++ src/Input.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useRef } from "react";
 
 interface IInput {
+    baseColor?: string,
     inputType: "email" | "password" | "cpf" | "text" | "submit",
     refInput?: React.RefObject<HTMLInputElement>,
 }
 
-const Input = ({inputType = "text", refInput }: IInput) => {
+const Input = ({inputType = "text", refInput, baseColor = "bg-slate-900/40" }: IInput) => {
     const ref = useRef<HTMLInputElement>(null);
     const [labelState, setlabelState] = useState(false);
     const [key, setKey] = useState("");
@@ -84,7 +85,7 @@ const Input = ({inputType = "text", refInput }: IInput) => {
         <div className="flex flex-col justify-center relative">
             <label className={`absolute transition-all ${labelState ? " -translate-y-11" : null} left-4 text-xl ${inputType === "submit" ? "hidden" : null} ${inputType === "cpf" ? "uppercase" : "capitalize"}`} htmlFor={inputType}>{inputType}</label>
             <input className="hidden" ref={refInput} type="text" />
-            <input maxLength={inputType === "cpf" ? 14 : undefined} type={inputType} value={inputType === "submit" ? inputType : undefined} onChange={e => handleChange(e)} onKeyDown={e => setKey(e.key)} onBlur={handleBlur} onFocus={handleFocus} ref={ref} className={` h-12 bg-slate-900/40 rounded-xl px-4 text-xl ${inputType === "submit" ? "cursor-pointer hover:bg-slate-900/60 transition-all" : null}`} id={inputType} />
+            <input maxLength={inputType === "cpf" ? 14 : undefined} type={inputType} value={inputType === "submit" ? inputType : undefined} onChange={e => handleChange(e)} onKeyDown={e => setKey(e.key)} onBlur={handleBlur} onFocus={handleFocus} ref={ref} className={` h-12 ${baseColor} rounded-xl px-4 text-xl ${inputType === "submit" ? "cursor-pointer hover:bg-slate-900/60 transition-all" : null}`} id={inputType} />
         </div>
     );
 }
