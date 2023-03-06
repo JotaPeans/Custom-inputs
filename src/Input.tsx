72d83ffcb9b7f431ba07e58728a4933d9a29daee
JotaPeans <jpfontesferreira1@gmail.com>
2023-03-05 23:03:34 -0300

    update input.tsx

--- src/Input.tsx
+++ src/Input.tsx
@@ -1,34 +1,90 @@
 import React, { useState, useRef } from "react";
 
 interface IInput {
-    label: string,
-    refInput: React.RefObject<HTMLInputElement>,
+    inputType: "email" | "password" | "cpf" | "text" | "submit",
+    refInput?: React.RefObject<HTMLInputElement>,
 }
 
-const Input = ({ label, refInput }: IInput) => {
+const Input = ({inputType = "text", refInput }: IInput) => {
     const ref = useRef<HTMLInputElement>(null);
-    const [labelState, setlabelState] = useState(false)
+    const [labelState, setlabelState] = useState(false);
+    const [key, setKey] = useState("");
 
     function handleFocus() {
-        ref.current?.value 
-        setlabelState(true);
+        if(inputType !== "submit") {
+            ref.current?.value;
+            setlabelState(true);
+        }
     }
 
     function handleBlur() {
-        !ref.current?.value ? setlabelState(false) : setlabelState(true);
+        if(inputType !== "submit") {
+            !ref.current?.value ? setlabelState(false) : setlabelState(true);
+        }
     }
 
-    function handleChange() {
-        if(refInput.current && ref.current) {
-            refInput.current.value = ref.current.value
+    function maskCpf(key: string) {
+        
+        const input = ref.current;
+
+        const numberKeys = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+        if(key in numberKeys) {
+            if(input?.value.length === 3 || input?.value.length === 7) {
+                input.value = input.value + ".";
+            }
+            else if(input?.value.length === 11) {
+                input.value = input.value + "-";
+            }
+            else if (input?.value.length === 4) {
+                let array = input.value.split("");
+                array[3] = ".";
+                let string = array.join("");
+                input.value = string + key;
+            }
+            else if (input?.value.length === 8) {
+                let array = input.value.split("");
+                array[7] = ".";
+                let string = array.join("");
+                input.value = string + key;
+            }
+            else if (input?.value.length === 12) {
+                let array = input.value.split("");
+                array[11] = "-";
+                let string = array.join("");
+                input.value = string + key;
+            }
+        }
+        else if(input && key !== "Backspace") {
+            let array = input.value.split("");
+            array.pop();
+            const string = array.join("");
+
+            input.value = string;
+        }
+
+    }
+
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        if(inputType === "cpf") {
+            maskCpf(key);
+
+            if(refInput?.current && ref.current) {
+                refInput.current.value = ref.current.value.replace(/\D/g, "");
+            }
+            return
+        }
+
+        if(refInput?.current && ref.current) {
+            refInput.current.value = ref.current.value;
         }
     }
 
     return (
         <div className="flex flex-col justify-center relative">
-            <label className={`absolute transition-all ${labelState ? " -translate-y-11" : null} left-4 text-xl`} htmlFor={label}>{label}</label>
+            <label className={`absolute transition-all ${labelState ? " -translate-y-11" : null} left-4 text-xl ${inputType === "submit" ? "hidden" : null} ${inputType === "cpf" ? "uppercase" : "capitalize"}`} htmlFor={inputType}>{inputType}</label>
             <input className="hidden" ref={refInput} type="text" />
-            <input onChange={handleChange} onBlur={handleBlur} onFocus={handleFocus} ref={ref} className=" h-12 bg-slate-900/40 rounded-xl px-4 text-xl" id={label} type="text" />
+            <input maxLength={inputType === "cpf" ? 14 : undefined} type={inputType} value={inputType === "submit" ? inputType : undefined} onChange={e => handleChange(e)} onKeyDown={e => setKey(e.key)} onBlur={handleBlur} onFocus={handleFocus} ref={ref} className={` h-12 bg-slate-900/40 rounded-xl px-4 text-xl ${inputType === "submit" ? "cursor-pointer hover:bg-slate-900/60 transition-all" : null}`} id={inputType} />
         </div>
     );
 }
